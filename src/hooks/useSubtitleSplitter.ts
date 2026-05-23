326adc35cc234c026044524505b4214d5265e2f2
tramcctw <tramcctw@foxmail.com>
2026-05-24 05:20:13 +0800

    feat: 优化

--- src/hooks/useSubtitleSplitter.ts
+++ src/hooks/useSubtitleSplitter.ts
@@ -1,10 +1,8 @@
 import { useMemo, useState } from 'react'
 import { splitSubtitleText } from '../utils/subtitleSplitter'
 
-const DEFAULT_TEXT = 'Most people waste hours every day without realizing it.'
-
 export function useSubtitleSplitter() {
-  const [inputText, setInputText] = useState(DEFAULT_TEXT)
+  const [inputText, setInputText] = useState('')
 
   const lines = useMemo(() => splitSubtitleText(inputText), [inputText])
   const outputText = useMemo(() => lines.join('\n\n'), [lines])
